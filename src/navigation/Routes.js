6f0fb50bf67d2954f37156c99eebdcb75a142285
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-01-13 23:15:27 +0300

    Router and Home page inserted

--- src/navigation/Routes.js
+++ src/navigation/Routes.js
@@ -8,13 +8,88 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 import IconMa from 'react-native-vector-icons/MaterialIcons';
 import IconMaC from 'react-native-vector-icons/MaterialCommunityIcons';
 import IconFa from 'react-native-vector-icons/FontAwesome5';
+
+import HomeScreen from '../screens/Home';
+
+const logo = {uri: 'https://www.opet.com.tr/assets/images/bg/logo.png'};
+
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
-export default App=()=> {  
+const NavigationDrawerStructure = props => {
+    const toggleDrawer = () => {
+      props.navigationProps.toggleDrawer();
+    };
+  
     return (
-      <SafeAreaView style={{flex:1,alignItems:'center',justifyContent:'center'}}>
-        <IconMa name='music' size={72} color="green" />
-      </SafeAreaView>
-    )
-  }
+      <TouchableOpacity style={{marginRight: 10}} onPress={() => toggleDrawer()}>
+        <IconMa name="menu" size={28} color="#fff" />
+      </TouchableOpacity>
+    );
+  };
+  
+  const HomeScreenStack = ({navigation}) => {
+    return (
+      <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{
+            title: '',
+            headerLeft: () => (
+              <Image
+                style={{width: 70, height: 24, marginLeft: 10}}
+                source={logo}
+              />
+            ),
+            headerRight: () => (
+              <NavigationDrawerStructure navigationProps={navigation} />
+            ),
+            headerTransparent: true,
+            headerTintColor: '#fff',
+          }}
+        />
+      </Stack.Navigator>
+    );
+  };
+
+const MyDrawer = () => {
+    return (
+      <Drawer.Navigator
+        drawerStyle={{
+          backgroundColor: '#0271cd',
+        }}
+        screenOptions={{ activeTintColor: '#fff', inactiveTintColor: '#fff', headerShown:false,
+          headerStyle: {  backgroundColor: '0271cd', borderBottomColor: '#fff', borderBottomWidth: 1, },
+          itemStyle: {marginVertical: 5},
+          labelStyle: {  fontSize: 18, },
+        }}>
+        <Drawer.Screen
+          name="HomeScreen1"
+          component={HomeScreenStack}
+          options={{
+            drawerLabel: 'Ana Sayfa',
+            drawerIcon: ({focused}) => (
+              <IconMa name="home" size={24} color="#fff" />
+            ),
+          }}
+        />
+  
+      </Drawer.Navigator>
+    );
+  };
+const Routes = () => {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="MyDrawer"
+            component={MyDrawer}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  };
+  
+  export default Routes;
---
+++ src/screens/Home.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import {SafeAreaView, Image, View, Text, TouchableOpacity} from 'react-native';
+import IconMa from 'react-native-vector-icons/MaterialIcons';
+import {deviceWidth, deviceHeight} from '../utils/dimensions';
+import {FlatListSlider} from 'react-native-flatlist-slider';
+
+const images = [
+  {
+    image:
+      'https://mobiloby.com/images/mobil-cozumler-large.jpg',
+    desc: 'Title 1',
+  },
+  {
+    image:
+      'https://mobiloby.com/images/web-uygulamalari-eticaret-large.jpg',
+    desc: 'Title 2',
+  },
+];
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: '#0271cd'}}>
+      <View style={{flex: 1}}>
+        <View
+          style={{
+            flex: 1,
+          }}>
+          <FlatListSlider
+            data={images}
+            height={deviceHeight / 2}
+            timer={5000}
+            onPress={item => alert(JSON.stringify(item))}
+            contentContainerStyle={{paddingHorizontal: 0}}
+            indicatorContainerStyle={{position: 'absolute', top: 0}}
+            indicatorActiveColor={'#e9bc18'}
+            indicatorInActiveColor={'#ffffff'}
+            indicatorActiveWidth={30}
+            animation
+          />        
+        </View>
+        <View style={{flex: 1, width: '100%', marginVertical: 20}}>
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <View
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRightWidth: 1,
+                borderRightColor: 'rgba(255, 255, 255, 0.2)',
+                borderBottomWidth: 1,
+                borderBottomColor: 'rgba(255, 255, 255, 0.2)',
+              }}>
+              <Text style={{color: '#fff', fontSize: 24}}>Aktif İlan Sayısı</Text>
+              <Text style={{color: '#fff', fontSize: 34}}> 5 </Text>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRightWidth: 1,
+                borderRightColor: 'rgba(255, 255, 255, 0.2)',
+                borderBottomWidth: 1,
+                borderBottomColor: 'rgba(255, 255, 255, 0.2)',
+              }}>
+              <Text style={{color: '#fff', fontSize: 24}}>İşe Yerleşen</Text>
+              <Text style={{color: '#fff', fontSize: 34}}> 1222 </Text>
+            </View>
+          </View>
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <View
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRightWidth: 1,
+                borderRightColor: 'rgba(255, 255, 255, 0.2)',
+              }}>
+              <Text style={{color: '#fff', fontSize: 24}}>Yayınlanan İlan</Text>
+              <Text style={{color: '#fff', fontSize: 34}}> 1227 </Text>
+            </View>
+         
+          </View>
+        </View>
+
+        <View
+          style={{
+            height: 60,
+            borderTopWidth: 1,
+            borderTopColor: 'rgba(255, 255, 255, 0.2)',
+            backgroundColor: '#0271cd',
+            width: '100%',
+            justifyContent: 'center',
+            padding: 10,
+            justifyContent: 'space-between',
+          }}>
+          <TouchableOpacity
+            style={{flexDirection: 'row'}}
+            onPress={() => navigation.navigate('KampanyaScreen1')}>
+            <View style={{flex: 1}}>
+              <Text style={{color: '#fff', fontSize: 18}}>İLANLAR</Text>
+            </View>
+            <View style={{flex: 1, alignItems: 'flex-end'}}>
+              <IconMa name="keyboard-arrow-right" size={32} color="#fff" />
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
